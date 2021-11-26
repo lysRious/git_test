6e86c4b5637808cbecee666b617c6d21321948be
lys <lys@qq.com>
2021-11-26 21:29:20 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author snkstart
+ * @creart 2021-11-26-21:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello version");
+    }
+}
